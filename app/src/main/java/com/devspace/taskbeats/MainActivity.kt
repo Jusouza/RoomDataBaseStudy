d8761552899663634cce05f01bad2cbef8e6d95f
Jusouza <jujussouza19@gmail.com>
2025-08-10 14:06:46 -0300

    Bottom sheet to create new category

--- app/src/main/java/com/devspace/taskbeats/MainActivity.kt
+++ app/src/main/java/com/devspace/taskbeats/MainActivity.kt
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private var categories = listOf<CategoryUiData>()
+    private var tasks = listOf<TaskUiData>()
 
     private val db by lazy {
         Room.databaseBuilder(
@@ -30,9 +34,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        insertDefaultCategory()
-        insertDefaultTask()
-
         val rvCategory = findViewById<RecyclerView>(R.id.rv_categories)
         val rvTask = findViewById<RecyclerView>(R.id.rv_tasks)
 
@@ -40,60 +41,40 @@ class MainActivity : AppCompatActivity() {
         val categoryAdapter = CategoryListAdapter()
 
         categoryAdapter.setOnClickListener { selected ->
-            val categoryTemp = categories.map { item ->
-                when {
-                    item.name == selected.name && !item.isSelected -> item.copy(isSelected = true)
-                    item.name == selected.name && item.isSelected -> item.copy(isSelected = false)
-                    else -> item
+
+            if (selected.name == "+") {
+                val createCategoryBottomSheet = CreateCategoryBottomSheet()
+
+                createCategoryBottomSheet.show(supportFragmentManager, "createCategoryBottomSheet")
+
+            } else {
+                val categoryTemp = categories.map { item ->
+                    when {
+                        item.name == selected.name && !item.isSelected -> item.copy(isSelected = true)
+                        item.name == selected.name && item.isSelected -> item.copy(isSelected = false)
+                        else -> item
+                    }
                 }
+
+                val taskTemp =
+                    if (selected.name != "ALL") {
+                        tasks.filter { it.category == selected.name }
+                    } else {
+                        tasks
+                    }
+                taskAdapter.submitList(taskTemp)
+                categoryAdapter.submitList(categoryTemp)
+
             }
-
-            val taskTemp =
-                if (selected.name != "ALL") {
-                    tasks.filter { it.category == selected.name }
-                } else {
-                    tasks
-                }
-            taskAdapter.submitList(taskTemp)
-
-            categoryAdapter.submitList(categoryTemp)
         }
 
         rvCategory.adapter = categoryAdapter
         getCategoriesFromDataBase(categoryAdapter)
 
         rvTask.adapter = taskAdapter
-        taskAdapter.submitList(tasks)
+        getTasksFromDataBase(taskAdapter)
     }
 
-    private fun insertDefaultCategory() {
-        // o map converte o UiData(CategoryList Mock) em category entity
-        val categoriesEntity = categories.map {
-            CategoryEntity(
-                name = it.name,
-                isSelected = it.isSelected
-            )
-        }
-        GlobalScope.launch(Dispatchers.IO) {
-            //TODO: Coroutines auxiliary o loading em background dos dados da tela.
-            categoryDao.insetAll(categoriesEntity)
-        }
-    }
-
-
-    private fun insertDefaultTask() {
-        // o map converte o UiData(CategoryList Mock) em category entity
-        val tasksEntity = tasks.map {
-            TaskEntity(
-                name = it.name,
-                category = it.category
-            )
-        }
-        GlobalScope.launch(Dispatchers.IO) { // roda em background por nao rodar na main thread
-            //TODO: Coroutines auxiliary o loading em background dos dados da tela.
-            taskDao.insetAll(tasksEntity)
-        }
-    }
 
     private fun getCategoriesFromDataBase(categoryListAdapter: CategoryListAdapter) {
         GlobalScope.launch(Dispatchers.IO) {
@@ -105,15 +86,37 @@ class MainActivity : AppCompatActivity() {
                     name = it.name,
                     isSelected = it.isSelected
                 )
+            }.toMutableList()
+            // add fake + category - tornar em lista mutavel para adicionar um novo item
+            categoriesUiData.add(
+                CategoryUiData(
+                    name = "+",
+                    isSelected = false
+                )
+            )
+            GlobalScope.launch(Dispatchers.Main) {
+                categories = categoriesUiData
+                //Aqui você envia essa lista já convertida para o Adapter (que vai exibir na tela).
+                categoryListAdapter.submitList(categoriesUiData)
             }
-            //Aqui você envia essa lista já convertida para o Adapter (que vai exibir na tela).
-            categoryListAdapter.submitList(categoriesUiData)
+
+        }
+    }
+
+    private fun getTasksFromDataBase(adapter: TaskListAdapter) {
+        GlobalScope.launch(Dispatchers.IO) {
+            val taskFromDb: List<TaskEntity> = taskDao.getAll()
+            val taskUiData = taskFromDb.map {
+                TaskUiData(
+                    name = it.name,
+                    category = it.category
+                )
+            }
+            GlobalScope.launch(Dispatchers.Main) {
+            tasks = taskUiData
+            adapter.submitList(taskUiData)
+            }
         }
     }
 
 }
-
-//val categories: List<CategoryUiData> = listOf()
-// Inserir as tasks na base de dados
-
-// val tasks: List<TaskUiData> = listOf()
